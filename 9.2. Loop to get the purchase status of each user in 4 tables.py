7ef44c284bac9d3bdfde9e48e6d56a9cd2f566ae
zy-yao <33860709+zy-yao@users.noreply.github.com>
2020-07-01 17:11:39 +0800

    Create 9.2. Loop to get the purchase status of each user in 4 tables.py

---
+++ 9.2. Loop to get the purchase status of each user in 4 tables.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import csv
+for i in range(0,4):
+    d = pd.read_csv('/Users/YAO/.spyder-py3/ForPython/new method/quater_sheets/user_bought_history_'+str(i+1)+'_standard.csv')     
+    with open('/Users/YAO/.spyder-py3/ForPython/new method/rule1_quarteresult/result'+str(i+1)+'_1.csv','w',newline='') as f:
+        writer = csv.writer(f)
+        for name,group in d.groupby('user_id'): 
+            line = [] 
+            for i in range(0,len(group)): 
+                line.append(group.iloc[i]['item_id'])
+            if len(line) > 1: 
+                writer.writerow(line)
