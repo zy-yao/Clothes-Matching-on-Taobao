04b22fcca6e1341184ee220c8c8e6b8a2b5f8da0
zy-yao <33860709+zy-yao@users.noreply.github.com>
2020-07-01 17:07:48 +0800

    Create 6.3. Loop to make the matching entries in the 365 tables correspond to each other in pairs.py

---
+++ 6.3. Loop to make the matching entries in the 365 tables correspond to each other in pairs.py
@@ -0,0 +1,16 @@
+import csv
+for i in range(0,365):
+    with open('/Users/YAO/.spyder-py3/ForPython/new method/rule1_result/result'+str(i+1)+'_1.csv') as origin_f:
+        with open('/Users/YAO/.spyder-py3/ForPython/new method/rule1_result2/result'+str(i+1)+'_2.csv','w',newline='') as new_f:
+            reader = csv.reader(origin_f)
+            writer = csv.writer(new_f)
+            writer.writerow(['testItem','matchItem'])
+            for row in reader:
+                for i in range(0,len(row)):
+                    for j in range(0,len(row)):
+                        line = []
+                        if i != j:
+                            line.append(row[i])  
+                            line.append(row[j])
+                            writer.writerow(line)
+
