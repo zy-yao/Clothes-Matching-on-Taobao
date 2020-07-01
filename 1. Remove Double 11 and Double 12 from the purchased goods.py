20db036b6e1d05e586e0f776880f729c79b0c011
zy-yao <33860709+zy-yao@users.noreply.github.com>
2020-07-01 17:06:44 +0800

    Create 1. Remove Double 11 and Double 12 from the purchased goods.py

---
+++ 1. Remove Double 11 and Double 12 from the purchased goods.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue April  1 16:01:40 2018
+
+@author: YAO
+"""
+import pandas as pd
+d= pd.read_table('用户历史行为user_bought_history.txt',sep='\s+',names = ['user_id','item_id','create_at'])
+dRemove11and12 = d[(d['create_at'] != 20141111) & (d['create_at'] != 20141212)]
+dRemove11and12.to_csv('user_bought_history_remove11and12.csv',index = False)
+
